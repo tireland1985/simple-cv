af6f95c2c7c0590e43d8fe0e5e3faa75cde6782e
tireland1985 <56227226+tireland1985@users.noreply.github.com>
2020-04-21 11:38:26 +0100

    ..

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,9 @@
 /* SCRIPTS.JS */
 
 function changePic() {
-    document.getElementById("profilePic").src = "scan.jpg";
+    document.getElementById("profilePic").src = "assets/scan.jpg";
   }
   
   function normalPic() {
-    document.getElementById("profilePic") .src = "19437590.jpg";
+    document.getElementById("profilePic") .src = "assets/19437590.jpg";
   }
